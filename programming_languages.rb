b4d0e9cc9a4db42d5858e089f02a14a5f012047a
Jose Elera <jelera@gmail.com>
2019-03-29 13:57:18 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
   # your code here
+  output = {}
+
+  languages.each do |lang_type, languages_list|
+    languages_list.each do |language, lang_properties|
+      output[language] = output[language] || {
+        type: lang_properties[:type],
+        style: []
+      }
+    end
+  end
+
+  output
 end
